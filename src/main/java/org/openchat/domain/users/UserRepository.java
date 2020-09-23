572458e6a814c34158422f710e587b3fa79fa34a
Alexandre Cuva <alexandre.cuva@gmail.com>
2020-09-23 18:22:37 +0200

    green

--- src/main/java/org/openchat/domain/users/UserRepository.java
+++ src/main/java/org/openchat/domain/users/UserRepository.java
@@ -1,11 +1,19 @@
 package org.openchat.domain.users;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+
+    List<User> users = new ArrayList<>();
+
     public void add(User user) {
-        throw new UnsupportedOperationException();
+        users.add(user);
     }
 
     public boolean isUsernameTaken(String username) {
-        return true;
+        return users
+                .stream().filter(user -> user.getUsername().equalsIgnoreCase(username))
+                .count() > 0;
     }
 }
--- src/test/java/org/openchat/domain/users/UserRepositoryShould.java
+++ src/test/java/org/openchat/domain/users/UserRepositoryShould.java
@@ -6,10 +6,13 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openchat.infrastructure.builders.UserBuilder.aUser;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserRepositoryShould {
 
+    public static final User ALICE = aUser().withUsername("Alice").build();
+    public static final User JOE = aUser().withUsername("Joe").build();
     private UserRepository userRepository;
 
     @Before
@@ -19,6 +22,9 @@ public class UserRepositoryShould {
     
     @Test
     public void imform_when_a_username_is_already_taken() {
-        assertThat(userRepository.isUsernameTaken("Alice")).isTrue();
+        userRepository.add(ALICE);
+
+        assertThat(userRepository.isUsernameTaken(ALICE.getUsername())).isTrue();
+        assertThat(userRepository.isUsernameTaken(JOE.getUsername())).isFalse();
     }
 }
